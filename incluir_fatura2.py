095bf09c1e0c486ef477aa181255b776b77e7e50
CaueFoyth <foythcaue@gmail.com>
2023-08-09 17:51:52 -0300

    A

--- incluir_fatura2.py
+++ incluir_fatura2.py
@@ -3,33 +3,18 @@
 import pyautogui
 import time
 
-x = 0
-while True:
-    x, y = pyautogui.locateCenterOnScreen('img/protheus.png', confidence=0.6)
-    if x != 0:
-        pyautogui.click(x, y)
-        x = 0
-        break
+x, y = pyautogui.locateCenterOnScreen('img/protheus.png', confidence=0.6)
+pyautogui.click(x, y)
 
-while True:
-    x, y = pyautogui.locateCenterOnScreen('img/recentes.png', confidence=0.8)
-    if x != 0:
-        pyautogui.click(x, y)
-        x = 0
-        break
+x, y = pyautogui.locateCenterOnScreen('img/recentes.png', confidence=0.8)
+pyautogui.click(x, y)
 
-while True:
-    x, y = pyautogui.locateCenterOnScreen('img/contas_pagar.png', confidence=0.9) 
-    if x != 0:
-        time.sleep(3)
-        pyautogui.click(x, y)
-        x = 0
-        break
+x, y = pyautogui.locateCenterOnScreen('img/contas_pagar.png', confidence=0.9) 
+time.sleep(3)
+pyautogui.click(x, y)
 
-while True:
+for i in range(10):
+    time.sleep(0.5)
     x, y = pyautogui.locateCenterOnScreen('img/apagar_click.png', confidence=0.7)
-    if x != 0:
-        pyautogui.click(x, y)
-        x = 0
-        break
-
+    pyautogui.click(x, y)
+    
