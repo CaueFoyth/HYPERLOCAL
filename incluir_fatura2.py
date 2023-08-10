ebcc8b7160c6901df70d23333cc4a46619f8dbfe
CaueFoyth <foythcaue@gmail.com>
2023-08-10 17:48:09 -0300

    ?

--- incluir_fatura2.py
+++ incluir_fatura2.py
@@ -2,6 +2,9 @@
 
 import pyautogui
 import time
+from os import path
+
+btn6_asset = path.dirname(path.realpath(__file__)) + 'img/apagar_click.png'
 
 x, y = pyautogui.locateCenterOnScreen('img/protheus.png', confidence=0.6)
 pyautogui.click(x, y)
@@ -13,8 +16,18 @@ x, y = pyautogui.locateCenterOnScreen('img/contas_pagar.png', confidence=0.9)
 time.sleep(3)
 pyautogui.click(x, y)
 
-for i in range(10):
+while True:
+    btn_found = pyautogui.locateAllOnScreen(btn6_asset, confidence=0.9)
+    if btn_found is not None:
+        x, y, *extras = btn_found
+        pyautogui.click(x,y)
+        break
+    else:
+        print("Imagem não encontrada")
     time.sleep(0.5)
-    x, y = pyautogui.locateCenterOnScreen('img/apagar_click.png', confidence=0.7)
-    pyautogui.click(x, y)
+
+# for i in range(10):
+#     time.sleep(0.5)
+#     x, y = pyautogui.locateCenterOnScreen('img/apagar_click.png', confidence=0.7)
+#     pyautogui.click(x, y)
     
