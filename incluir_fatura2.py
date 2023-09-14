d5712038989cc0cd3c473e75fe1b091d5f5ea891
CaueFoyth <foythcaue@gmail.com>
2023-09-14 17:44:31 -0300

    While update

--- incluir_fatura2.py
+++ incluir_fatura2.py
@@ -2,32 +2,49 @@
 
 import pyautogui
 import time
-from os import path
 
-btn6_asset = path.dirname(path.realpath(__file__)) + 'img/apagar_click.png'
-
-x, y = pyautogui.locateCenterOnScreen('img/protheus.png', confidence=0.6)
-pyautogui.click(x, y)
-
-x, y = pyautogui.locateCenterOnScreen('img/recentes.png', confidence=0.8)
-pyautogui.click(x, y)
-
-x, y = pyautogui.locateCenterOnScreen('img/contas_pagar.png', confidence=0.9) 
-time.sleep(3)
-pyautogui.click(x, y)
+pyautogui.useImageNotFoundException()
 
 while True:
-    btn_found = pyautogui.locateAllOnScreen(btn6_asset, confidence=0.9)
-    if btn_found is not None:
-        x, y, *extras = btn_found
-        pyautogui.click(x,y)
+    try:
+        x, y = pyautogui.locateCenterOnScreen('img/protheus.png', confidence=0.9)
+        pyautogui.click(x, y)
         break
-    else:
-        print("Imagem não encontrada")
-    time.sleep(0.5)
+    except pyautogui.ImageNotFoundException:
+        pass
 
-# for i in range(10):
-#     time.sleep(0.5)
-#     x, y = pyautogui.locateCenterOnScreen('img/apagar_click.png', confidence=0.7)
-#     pyautogui.click(x, y)
-    
+
+# x, y = pyautogui.locateCenterOnScreen('img/protheus.png', confidence=0.9)
+# pyautogui.click(x, y)
+
+while True:
+    try:
+        x, y = pyautogui.locateCenterOnScreen('img/recentes.png', confidence=0.8)
+        pyautogui.click(x, y)
+        break
+    except pyautogui.ImageNotFoundException:
+        pass
+
+# x, y = pyautogui.locateCenterOnScreen('img/recentes.png', confidence=0.8)
+# pyautogui.click(x, y)
+# time.sleep(1)
+
+while True:
+    try:
+        x, y = pyautogui.locateCenterOnScreen('img/contas_pagar.png', confidence=0.9) 
+        time.sleep(2)
+        pyautogui.click(x, y)
+        break
+    except pyautogui.ImageNotFoundException:
+        pass
+
+while True:
+    try:
+        x, y = pyautogui.locateCenterOnScreen('img/apagar_click.png', confidence=0.7) 
+        pyautogui.click(x, y)
+        break
+    except pyautogui.ImageNotFoundException:
+        pass
+
+pyautogui.press("tab")
+pyautogui.press("enter")     
